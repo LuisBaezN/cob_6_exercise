98a72ecfec9d291d810141bd0bdeeb773583a6f7
Luis Angel Baez Nieto <baez.201041081@gmail.com>
2024-01-31 08:21:54 -0600

    add: update

--- 6EmpNom.cbl
+++ 6EmpNom.cbl
@@ -7,8 +7,28 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. EmpNom.
        ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT MOVIM ASSIGN TO DISK.
+           SELECT EMPINX ASSIGN TO DISK
+                           ORGANISATION IS INDEXED
+                           ACCESS MODE IS DYNAMIC
+                           RECORD KEY IS EMI-NOMI.
        DATA DIVISION.
        FILE SECTION.
+       FD  MOVIM.
+       01  MOV-REG.
+           02  MOV-NOMI    PIC 9(06).
+           02  MOV-CLAV    PIC 9(14).
+           02  MOV-IMP     PIC 9(05)V99.
+           02  FILLER      PIC XX.
+       FD  EMPINX.
+       01  EMI-REG.
+           02 EMI-NOMI PIC 9(06).
+           02 EMI-NOMB PIC X(15).
+           02 EMI-DEPT PIC X(03).
+           02 EMI-SUEL PIC 9(05)V99.
+           02 FILLER   PIC XX.
        WORKING-STORAGE SECTION.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
@@ -18,11 +38,23 @@
 
        100-START.
            DISPLAY "> Running start...".
+           OPEN INPUT MOVIM.
+           OPEN I-O EMPINX.
+           READ MOVIM.
+
 
        200-PROCESS.
            DISPLAY "> Running process...".
+           READ MOVIM.
+      *     DISPLAY MOV-NOMI, ", ", MOV-CLAV, ", ", MOV-IMP.
+           MOVE MOV-NOMI TO EMI-NOMI.
+           READ EMPINX
+           DISPLAY MOV-REG.
+           DISPLAY EMI-REG.
 
        300-END.
            DISPLAY "> Running end...".
+           CLOSE MOVIM.
+           CLOSE EMPINX.
            STOP RUN.
        END PROGRAM EmpNom.
---
+++ movimientos.cbl
@@ -0,0 +1,83 @@
+      ******************************************************************
+      * Author: Luis Angel Baez Nieto
+      * Date:
+      * Purpose: Genera diversos movimientos de diferentes empleados
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MOVIMIENTOS.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT MOVIM ASSIGN TO DISK.
+       DATA DIVISION.
+       FILE SECTION.
+       FD  MOVIM.
+       01  MOV-REG.
+           02  MOV-NOMI    PIC 9(6).
+           02  MOV-CLAV    PIC X(14).
+           02  MOV-IMP     PIC 9(05)V99.
+           02  FILLER      PIC XX.
+       WORKING-STORAGE SECTION.
+       77  I               PIC 999.
+       77  N-RAND1         PIC 9(19).
+       77  N-RAND2         PIC 9(19).
+       77  CANT            PIC 9(19)V99.
+       01  CLAVE-TABLA.
+           02 CLAVE-ROW OCCURS 11 TIMES.
+               03 CLAVE  PIC X(13).
+       01  NOMINA-TABLA.
+           02 NOMI-ROW OCCURS 11 TIMES.
+               03  NOMI-D  PIC 9(6).
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           PERFORM INICIALIZACION.
+           OPEN OUTPUT MOVIM.
+           PERFORM VARYING I FROM 1 BY 1 UNTIL I = 102
+               PERFORM GENERATE-RAND
+               MOVE NOMI-D(N-RAND1) TO MOV-NOMI
+               MOVE CLAVE(N-RAND2) TO MOV-CLAV
+               MOVE CANT TO MOV-IMP
+               WRITE MOV-REG
+           END-PERFORM
+           CLOSE MOVIM
+           STOP RUN.
+
+       INICIALIZACION.
+           MOVE "Sueldo       "    TO CLAVE(1).
+           MOVE "Bono         "    TO CLAVE(2).
+           MOVE "Puntualidad  "    TO CLAVE(3).
+           MOVE "Productividad"    TO CLAVE(4).
+           MOVE "Prestamo     "    TO CLAVE(5).
+           MOVE "Horas Extras "    TO CLAVE(6).
+           MOVE "Impuestos    "    TO CLAVE(7).
+           MOVE "IMSS         "    TO CLAVE(8).
+           MOVE "Faltas       "    TO CLAVE(9).
+           MOVE "Pago prestamo"    TO CLAVE(10).
+           MOVE "Vales        "    TO CLAVE(11).
+
+           MOVE 32421              TO NOMI-D(1).
+           MOVE 87343              TO NOMI-D(2).
+           MOVE 23491              TO NOMI-D(3).
+           MOVE 45861              TO NOMI-D(4).
+           MOVE 78562              TO NOMI-D(5).
+           MOVE 98752              TO NOMI-D(6).
+           MOVE 12348              TO NOMI-D(7).
+           MOVE 22215              TO NOMI-D(8).
+           MOVE 12345              TO NOMI-D(9).
+           MOVE 13135              TO NOMI-D(10).
+           MOVE 44444              TO NOMI-D(11).
+
+       GENERATE-RAND.
+           COMPUTE N-RAND1 = FUNCTION RANDOM(I) * 10000000000000000.
+           COMPUTE N-RAND1 = FUNCTION MOD(N-RAND1, 11) + 1.
+
+           COMPUTE N-RAND2 = FUNCTION RANDOM(N-RAND1)*10000000000000000.
+           COMPUTE N-RAND2 = FUNCTION MOD(N-RAND2, 11) + 1.
+
+           COMPUTE CANT = FUNCTION RANDOM(N-RAND2) * 10000000000000000.
+           COMPUTE CANT = FUNCTION MOD(CANT, 99999).
+           COMPUTE CANT = CANT + (N-RAND1 + N-RAND2)/100.
+
+       END PROGRAM MOVIMIENTOS.
